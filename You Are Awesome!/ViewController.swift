b33dd18f3ed3350eb73620a31b189cb6233d98f0
yeindaw <yedawin@gmail.com>
2020-01-20 22:58:24 -0500

    completed 2.7

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -16,12 +16,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         messageLabel.text = ""
     }
-
-    @IBAction func messageButtonPressed(_ sender: UIButton) {
-        messageLabel.text =  "YOU ARE AWESOME!"
-        imageView.image = UIImage(named: "image0")
-        
-    }
     
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        let awesomeMessage = "YOU ARE AWESOME"
+        let greatMessage = "YOU ARE GREAT!"
+        let coolMessage = "YOU ARE COOL"
+        if messageLabel.text == awesomeMessage {
+            messageLabel.text = greatMessage
+            imageView.image = UIImage(named: "image1")
+        } else if messageLabel.text == greatMessage {
+            messageLabel.text = coolMessage
+            imageView.image = UIImage(named: "image2")
+        } else {
+            messageLabel.text = awesomeMessage
+            imageView.image = UIImage(named: "image0")
+            
+        }
+    }
 }
-
